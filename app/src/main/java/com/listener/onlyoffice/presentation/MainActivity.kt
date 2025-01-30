604793cfb7a07baa3f5249afd02ae6053d686e8c
Георгий Егор <egor.vorontsov03@mail.ru>
2025-01-30 21:25:04 +0400

    Moved to other place

--- app/src/main/java/com/listener/onlyoffice/presentation/MainActivity.kt
+++ app/src/main/java/com/listener/onlyoffice/presentation/MainActivity.kt
@@ -1,10 +1,12 @@
-package com.listener.onlyoffice
+package com.listener.onlyoffice.presentation
 
 import android.os.Bundle
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.ViewCompat
-import androidx.core.view.WindowInsetsCompat
+import com.listener.onlyoffice.DI
+import com.listener.onlyoffice.R
+import com.listener.onlyoffice.data.remote.retrofit.RetrofitStorage
+import com.listener.onlyoffice.data.repository.AuthRepositoryImpl
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
