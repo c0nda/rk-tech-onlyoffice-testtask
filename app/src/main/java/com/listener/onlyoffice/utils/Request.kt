e2a65c178c4ecd6959cf36a5d53b611726315f85
Георгий Егор <egor.vorontsov03@mail.ru>
2025-01-30 21:25:41 +0400

    Added request handler

---
+++ app/src/main/java/com/listener/onlyoffice/utils/Request.kt
@@ -0,0 +1,7 @@
+package com.listener.onlyoffice.utils
+
+sealed class Request<T> {
+    class Loading<T> : Request<T>()
+    data class Success<T>(val data: T) : Request<T>()
+    data class Error<T>(val error: Throwable?) : Request<T>()
+}
