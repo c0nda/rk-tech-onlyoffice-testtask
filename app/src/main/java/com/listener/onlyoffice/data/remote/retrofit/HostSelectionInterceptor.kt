6d10dbe5715e3e0caeaf58f3de9fb19b20760374
Георгий Егор <egor.vorontsov03@mail.ru>
2025-01-31 20:42:10 +0400

    Fixed bug with url structure

--- app/src/main/java/com/listener/onlyoffice/data/remote/retrofit/HostSelectionInterceptor.kt
+++ app/src/main/java/com/listener/onlyoffice/data/remote/retrofit/HostSelectionInterceptor.kt
@@ -1,26 +1,31 @@
 package com.listener.onlyoffice.data.remote.retrofit
 
-import okhttp3.HttpUrl
+import okhttp3.HttpUrl.Companion.toHttpUrlOrNull
 import okhttp3.Interceptor
 import okhttp3.Response
 import java.io.IOException
 
 object HostSelectionInterceptor : Interceptor {
 
-    @Volatile
     private var host: String? = null
+    private var scheme: String? = null
 
-    fun setNewHost(newHost: String) {
-        host = newHost
+    fun setNewHost(newUrl: String) {
+        val url = newUrl.toHttpUrlOrNull()
+        scheme = url?.scheme
+        host = url?.host
     }
 
     @Throws(IOException::class)
     override fun intercept(chain: Interceptor.Chain): Response {
         var request = chain.request()
-        if (host != null) {
-            val newUrl = HttpUrl.parse(host!!)
+        if (host != null && scheme != null) {
+            val newUrl = request.url.newBuilder()
+                .scheme(scheme!!)
+                .host(host!!)
+                .build()
             request = request.newBuilder()
-                .url(newUrl!!)
+                .url(newUrl)
                 .build()
         }
         return chain.proceed(request)
