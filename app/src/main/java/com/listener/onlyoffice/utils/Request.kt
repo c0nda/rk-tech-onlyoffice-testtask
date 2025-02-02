d081c616550fd1260c04c42492cb38b98aca5e36
Георгий Егор <egor.vorontsov03@mail.ru>
2025-02-02 13:23:59 +0400

    Added init state

--- app/src/main/java/com/listener/onlyoffice/utils/Request.kt
+++ app/src/main/java/com/listener/onlyoffice/utils/Request.kt
@@ -1,6 +1,8 @@
 package com.listener.onlyoffice.utils
 
 sealed class Request<T> {
+
+    class Init<T> : Request<T>()
     class Loading<T> : Request<T>()
     data class Success<T>(val data: T) : Request<T>()
     data class Error<T>(val error: Throwable?) : Request<T>()
