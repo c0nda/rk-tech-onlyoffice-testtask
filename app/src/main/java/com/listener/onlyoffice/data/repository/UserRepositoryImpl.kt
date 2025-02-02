5751d3ce33e1c4ab9b633f06810bf9b8e76ce183
Георгий Егор <egor.vorontsov03@mail.ru>
2025-02-02 13:20:48 +0400

    AuthRepository -> UserRepository

---
+++ app/src/main/java/com/listener/onlyoffice/data/repository/UserRepositoryImpl.kt
@@ -0,0 +1,30 @@
+package com.listener.onlyoffice.data.repository
+
+import com.listener.onlyoffice.data.remote.RemoteDataSource
+import com.listener.onlyoffice.domain.model.AccessToken
+import com.listener.onlyoffice.domain.model.UserProfile
+import com.listener.onlyoffice.domain.repository.UserRepository
+import com.listener.onlyoffice.utils.Request
+import kotlinx.coroutines.flow.Flow
+import javax.inject.Inject
+
+class UserRepositoryImpl @Inject constructor(
+    private val remoteDataSource: RemoteDataSource
+) : UserRepository {
+
+    override suspend fun doAuth(email: String, password: String): Flow<Request<AccessToken>> {
+        return remoteDataSource.authenticate(email, password)
+    }
+
+    override suspend fun checkAuth(): Flow<Request<Boolean>> {
+        return remoteDataSource.checkAuth()
+    }
+
+    override suspend fun getUserProfile(): Flow<Request<UserProfile>> {
+        return remoteDataSource.getUserProfile()
+    }
+
+    override suspend fun logoutUser(): Flow<Request<Unit>> {
+        return remoteDataSource.logoutUser()
+    }
+}
