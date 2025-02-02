13c69ba936efe1bd44b8eaebef5705bf61c21b4a
Георгий Егор <egor.vorontsov03@mail.ru>
2025-02-02 13:24:49 +0400

    Added new extensions

--- app/src/main/java/com/listener/onlyoffice/data/Mapper.kt
+++ app/src/main/java/com/listener/onlyoffice/data/Mapper.kt
@@ -1,8 +1,21 @@
 package com.listener.onlyoffice.data
 
-import com.listener.onlyoffice.data.remote.dto.AccessTokenDTO
+import com.listener.onlyoffice.data.remote.dto.CheckAuthDTO
+import com.listener.onlyoffice.data.remote.dto.ResponseTokenDTO
+import com.listener.onlyoffice.data.remote.dto.ResponseUserProfileDTO
+import com.listener.onlyoffice.data.remote.dto.UserProfileDTO
 import com.listener.onlyoffice.domain.model.AccessToken
+import com.listener.onlyoffice.domain.model.UserProfile
 
-fun AccessTokenDTO.toDomain() = AccessToken(
-    token = token
+fun ResponseTokenDTO.toDomain() = AccessToken(
+    token = response.token
 )
+
+fun ResponseUserProfileDTO.toDomain() = UserProfile(
+    id = response.id,
+    displayName = response.displayName,
+    avatar = response.avatar,
+    email =response.email
+)
+
+fun CheckAuthDTO.toDomain()  = response
